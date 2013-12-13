10262840b616cc05d42835c0315ba3f1008cea2a
Hector G. Parra <hector@hectorparra.com>
2013-12-13 01:59:23 -0800

    fix(grunt): run even if there are no files
    Conditional to prevent background-imager from running if grunt did not find compatible files actually causes grunt to terminate abruptly, without warning.

--- tasks/background_imager.js
+++ tasks/background_imager.js
@@ -22,36 +22,32 @@ module.exports = function(grunt) {
 		var done = this.async();
 		this.files.forEach(function(file) {
 
-			// only run if there are files
-			if (file.src.length > 0) {
+			var src = file.src.filter(function (filepath) {
+				// HACK
+				// we usd file.cwd even though expand should be false
+				if (!grunt.file.exists(file.cwd, filepath)) {
+					grunt.log.warn("Source file \"" + filepath + "\" not found.");
+					return false;
+				} else {
+					return true;
+				}
+			});
 
-				var src = file.src.filter(function (filepath) {
-					// HACK
-					// we usd file.cwd even though expand should be false
-					if (!grunt.file.exists(file.cwd, filepath)) {
-						grunt.log.warn("Source file \"" + filepath + "\" not found.");
-						return false;
-					} else {
-						return true;
-					}
-				});
+			var success = true;
 
-				var success = true;
+			// create media rules
+			bgi.createMediaRules(src, file.cwd, options, function(err, mediaRules) {
 
-				// create media rules
-				bgi.createMediaRules(src, file.cwd, options, function(err, mediaRules) {
+				if (err) {
+					grunt.fail.warn(err);
+					success = false;
+				}
 
-					if (err) {
-						grunt.fail.warn(err);
-						success = false;
-					}
+				grunt.file.write(file.dest, bgi.generateCSS(mediaRules, options.tabSpacing));
+				grunt.log.writeln("File \"" + file.dest.toString().cyan + "\" created.");
 
-					grunt.file.write(file.dest, bgi.generateCSS(mediaRules, options.tabSpacing));
-					grunt.log.writeln("File \"" + file.dest.toString().cyan + "\" created.");
-
-					done(success);
-				});
-			}
+				done(success);
+			});
 		});
 	});
 };
